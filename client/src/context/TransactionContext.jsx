723f3b2f5ec323920f925908f25637240d564595
giftea <eleojogiftuhiene@gmail.com>
2022-01-14 05:51:32 +0100

    added getAllTransacion func to context

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -32,6 +32,33 @@ export const TransactionProvider = ({ children }) => {
         setFormData((prev) => ({...prev, [name]: e.target.value}));
     }
 
+    const getAllTransactions = async () => {
+        try {
+          if (ethereum) {
+            const transactionsContract = createEthereumContract();
+    
+            const availableTransactions = await transactionsContract.getAllTransactions();
+    
+            const structuredTransactions = availableTransactions.map((transaction) => ({
+              addressTo: transaction.receiver,
+              addressFrom: transaction.sender,
+              timestamp: new Date(transaction.timestamp.toNumber() * 1000).toLocaleString(),
+              message: transaction.message,
+              keyword: transaction.keyword,
+              amount: parseInt(transaction.amount._hex) / (10 ** 18)
+            }));
+    
+            console.log(structuredTransactions);
+    
+            setTransactions(structuredTransactions);
+          } else {
+            console.log("Ethereum is not present");
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      };
+
     const checkIfWalletIsConnected = async ( )=> {
 
         try {
@@ -51,6 +78,21 @@ export const TransactionProvider = ({ children }) => {
             throw new Error("No ethereum object")
         }
     }
+
+    const checkIfTransactionsExists = async () => {
+        try {
+          if (ethereum) {
+            const transactionsContract = createEthereumContract();
+            const currentTransactionCount = await transactionsContract.getTransactionCount();
+    
+            window.localStorage.setItem("transactionCount", currentTransactionCount);
+          }
+        } catch (error) {
+          console.log(error);
+    
+          throw new Error("No ethereum object");
+        }
+      };
 
     const connectWallet = async () => {
         try {
